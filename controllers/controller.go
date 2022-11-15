190d6c1015cf212851452b863fb3e7de70facc7f
Bryan Keane <bkeane@redhat.com>
2022-11-15 20:27:12 +0000

    Finished rbac-management-operator

--- controllers/controller.go
+++ controllers/controller.go
@@ -44,6 +44,7 @@ const (
 	serviceAccountName       = "rbac-manager"
 	roleName                 = "rbac-role"
 	roleBindingName          = "rbac-role-binding"
+	namespace                = "rbac-term-paper"
 )
 
 var _ reconcile.Reconciler = &DeploymentController{}
@@ -60,7 +61,6 @@ func (p DeploymentController) Add(mgr manager.Manager) error {
 		return err
 	}
 
-	// TODO Look for resources asking for permission to change a deployment with the label rbac-manager-request: deployment-name
 	labelSelectorPredicate, err := predicate.LabelSelectorPredicate(
 		v1.LabelSelector{
 			MatchLabels: map[string]string{
@@ -73,37 +73,38 @@ func (p DeploymentController) Add(mgr manager.Manager) error {
 		return err
 	}
 
-	// Add a watch to Deployments containing that label
+	// Add a watch to Deployments containing that label in the namespace
 	err = c.Watch(
-		&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForObject{}, labelSelectorPredicate)
+		&source.Kind{Type: &appsv1.Deployment{
+			ObjectMeta: v1.ObjectMeta{Namespace: namespace},
+		}}, &handler.EnqueueRequestForObject{}, labelSelectorPredicate)
 	if err != nil {
 		logrus.Errorf("error creating watch for deployments: %v", err)
 		return err
 	}
 
-	err = c.Start(context.Background())
-
 	return nil
 }
 
-//+kubebuilder:rbac:groups=core,resources=deployment,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 
 func (p DeploymentController) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
 	logrus.Infof("Reconciling deployment %s", request.NamespacedName)
 
-	var deployment appsv1.Deployment
 	serviceAccount := corev1.ServiceAccount{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      serviceAccountName,
 			Namespace: request.Namespace,
 		},
 	}
+
 	role := rbacv1.Role{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      roleName,
 			Namespace: request.Namespace,
 		},
 	}
+
 	roleBinding := rbacv1.RoleBinding{
 		ObjectMeta: v1.ObjectMeta{
 			Name:      roleBindingName,
@@ -113,6 +114,8 @@ func (p DeploymentController) Reconcile(ctx context.Context, request reconcile.R
 
 	// TODO Deployment Logic
 
+	var deployment appsv1.Deployment
+
 	// Get the requester deployment
 	if err := p.Client.Get(ctx, request.NamespacedName, &deployment); err != nil {
 		if errors.IsNotFound(err) {
@@ -120,12 +123,12 @@ func (p DeploymentController) Reconcile(ctx context.Context, request reconcile.R
 		}
 		return reconcile.Result{}, err
 	}
+	logrus.Infof("Got deployment %s", deployment.Name)
 
 	// TODO Service Account Logic
 
 	// Check if service account is already assigned
 	if deployment.Spec.Template.Spec.ServiceAccountName != serviceAccountName {
-
 		// Check if the service account exists
 		if err := p.Client.Get(ctx, client.ObjectKeyFromObject(&serviceAccount), &corev1.ServiceAccount{}); err != nil {
 			if errors.IsNotFound(err) {
@@ -136,23 +139,29 @@ func (p DeploymentController) Reconcile(ctx context.Context, request reconcile.R
 						Namespace: request.Namespace,
 					},
 				}); err != nil {
+					logrus.Errorf("failed to create service account: %v", err)
 					return reconcile.Result{}, err
 				}
+				logrus.Infof("created service account %s", serviceAccountName)
 			}
-			return reconcile.Result{}, err
-		}
-
-		// Assign the service account to the requester
-		deployment.Spec.Template.Spec.ServiceAccountName = serviceAccountName
-		if err := p.Client.Update(ctx, &deployment); err != nil {
+			logrus.Errorf("error getting service account %s: %v", serviceAccountName, err)
 			return reconcile.Result{}, err
 		}
 	}
+	logrus.Infof("got service account %s", serviceAccountName)
+
+	// Assign the service account to the requester
+	deployment.Spec.Template.Spec.ServiceAccountName = serviceAccountName
+	if err := p.Client.Update(ctx, &deployment); err != nil {
+		logrus.Errorf("failed to update deployment with new service account: %v", err)
+		return reconcile.Result{}, err
+	}
+	logrus.Infof("updated deployment %s with service account %s", deployment.Name, serviceAccountName)
 
 	// TODO Role Logic
 
 	// Check if the role already exists
-	if err := p.Client.Get(ctx, request.NamespacedName, &role); err != nil {
+	if err := p.Client.Get(ctx, client.ObjectKeyFromObject(&role), &role); err != nil {
 		if errors.IsNotFound(err) {
 			// Create role
 			if err := p.Client.Create(ctx, &rbacv1.Role{
@@ -165,21 +174,25 @@ func (p DeploymentController) Reconcile(ctx context.Context, request reconcile.R
 						APIGroups:     []string{"apps"},
 						Resources:     []string{"deployments"},
 						Verbs:         []string{"get", "list", "watch", "create", "update", "patch", "delete"},
-						ResourceNames: []string{deployment.Labels["rbac-management-operator.com/access"]},
+						ResourceNames: []string{"nginx-deployment"},
 					},
 				},
 			}); err != nil {
+				logrus.Errorf("failed to create role: %v", err)
 				return reconcile.Result{}, err
 			}
+			logrus.Infof("created role %s", roleName)
 		}
+		logrus.Errorf("error getting role %s: %v", roleName, err)
+		return reconcile.Result{}, err
 	}
+	logrus.Infof("got role %s", roleName)
 
 	// TODO Role Binding Logic
 
 	// Check if the role binding already exists
-	if err := p.Client.Get(ctx, request.NamespacedName, &roleBinding); err != nil {
+	if err := p.Client.Get(ctx, client.ObjectKeyFromObject(&roleBinding), &roleBinding); err != nil {
 		if errors.IsNotFound(err) {
-
 			// Create role binding
 			if err := p.Client.Create(ctx, &rbacv1.RoleBinding{
 				ObjectMeta: v1.ObjectMeta{
@@ -199,11 +212,15 @@ func (p DeploymentController) Reconcile(ctx context.Context, request reconcile.R
 					APIGroup: "rbac.authorization.k8s.io",
 				},
 			}); err != nil {
+				logrus.Errorf("failed to create role binding: %v", err)
 				return reconcile.Result{}, err
 			}
+			logrus.Infof("created role binding %s", roleBindingName)
 		}
+		logrus.Errorf("error getting role binding %s: %v", roleBindingName, err)
 		return reconcile.Result{}, err
 	}
+	logrus.Infof("got role binding %s", roleBindingName)
 
 	return reconcile.Result{}, nil
 }
